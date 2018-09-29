e5f009ff641d419b1ad49d602a66ffe66b92c253
hartyp <hartyp@tcd.ie>
2018-09-29 12:42:03 -0700

    Second Commit

---
+++ LCA-BTree/src/BT_NoParentPtr_Solution1Test.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+public class BT_NoParentPtr_Solution1Test {
+	
+	 @Test
+	    public void testConstructor()
+	    {
+	        new BT_NoParentPtr_Solution1();
+	    }
+
+}
