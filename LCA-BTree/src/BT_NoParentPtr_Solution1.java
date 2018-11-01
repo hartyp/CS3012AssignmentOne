3dbaef3d9c4e81ab80127a9e81ee7d91de4e7295
hartyp <hartyp@tcd.ie>
2018-11-01 17:48:00 +0000

    DAG extended - added more methods

--- LCA-BTree/src/BT_NoParentPtr_Solution1.java
+++ LCA-BTree/src/BT_NoParentPtr_Solution1.java
@@ -52,6 +52,87 @@ public class BT_NoParentPtr_Solution1 {
 			return this.key.toString();
 		}
 	}
+	
+	public static ArrayList<Node> getLCA(ArrayList<Node> listOfNodes, Node x, Node y) {
+		//Check if it is acyclic
+	
+		
+
+	}
+
+	private static boolean lookForImpasse(ArrayList<Node> commonAncestors) {
+		for (int i = 0; i < commonAncestors.size(); i++) {
+			Node tmp = commonAncestors.get(i);
+			if (overlap(commonAncestors, tmp.edgesTo).size() != 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void find(Node root, ArrayList<Node> ancesX, ArrayList<Node> ancesY) {
+		if (root.edgesTo == null || root.edgesTo.size() == 0)
+			return;
+
+		for (int i = 0; i < root.edgesTo.size(); i++) {
+			Node tmp = (Node) root.edgesTo.get(i);
+			if (!(ancesX.contains(tmp) || ancesY.contains(tmp)))
+				find(tmp, ancesX, ancesY);
+			if (ancesX.contains(tmp))
+				ancesX.add(root);
+			if (ancesY.contains(tmp))
+				ancesY.add(root);
+		}
+	}
+
+	public static boolean acyclicTest(ArrayList<Node> list) {
+
+		if (list == null || list.size() == 0)
+			return true;
+
+		for (int i = 0; i < list.size(); i++) {
+			ArrayList<Node> inspected = new ArrayList<Node>();
+			ArrayList<Node> stack = new ArrayList<Node>();
+			Node tmp = list.get(i);
+			boolean cyclical = false;
+			cyclical = checkCycle(list, tmp, inspected, stack, cyclical);
+			if (cyclical) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static boolean checkCycle(ArrayList<Node> list, Node tmp, ArrayList<Node> inspected, ArrayList<Node> stack,
+			boolean cyclical) {
+		inspected.add(tmp);
+		stack.add(tmp);
+
+		for (int i = 0; i < tmp.edgesTo.size(); i++) {
+			Node a = (Node) tmp.edgesTo.get(i);
+			if (!inspected.contains(a)) {
+				cyclical = cyclical || checkCycle(list, a, inspected, stack, cyclical);
+			} else if (stack.contains(a)) {
+				cyclical = true;
+				return cyclical;
+			}
+		}
+		stack.remove(tmp);
+		return cyclical;
+	}
+
+
+	public static ArrayList<Node> overlap(ArrayList<Node> firstList, ArrayList<Node> secondList) {
+		ArrayList<Node> list = new ArrayList<Node>();
+
+		for (Node n : firstList) {
+			if (secondList.contains(n)) {
+				list.add(n);
+			}
+		}
+
+		return list;
+	}
 
 
 }
